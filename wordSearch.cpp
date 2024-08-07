6f84439b23efc54b97d7abceee744cc12826b31d
AdithyaSakthimani <160578687+AdithyaSakthimani@users.noreply.github.com>
2024-08-07 09:59:29 +0530

    Update wordSearch.cpp
    Problem Description : 
    You have a 2D grid of characters and a list of words. For each word in the list, you need to determine whether the word can be found in the grid. The word can be found in any of the following directions:
    
    Horizontally (left-to-right)
    Vertically (top-to-bottom)
    Diagonally (in all four possible diagonal directions)
    If the word is found, return the coordinates of the characters forming the word in the grid. If not found, return (-1, -1).
    
    Solution Description
    The solution involves two main steps:
    
    Searching in Straight Directions (Horizontal and Vertical)
    
    For each word, iterate over every cell in the grid.
    If the starting character of the word matches the cell, perform a Breadth-First Search (BFS) to find the word in straight directions (left-to-right, right-to-left, top-to-bottom, bottom-to-top).
    Use a queue to explore all possible positions in the four straight directions.
    If the word is found, store the coordinates and mark the word as found.
    Searching in Diagonal Directions
    
    If the word was not found in the straight directions, perform a diagonal search.
    For each cell in the grid, check if the starting character matches.
    If so, search in each of the four diagonal directions (top-left to bottom-right, top-right to bottom-left, bottom-left to top-right, bottom-right to top-left).
    Use a loop to explore cells in the diagonal direction until the end of the grid or the end of the word is reached.
    If the word is found, store the coordinates else print not found .

--- wordSearch.cpp
+++ wordSearch.cpp
@@ -1,80 +1,116 @@
 #include <bits/stdc++.h>
-using namespace std ; 
+using namespace std;
 
-vector<vector<pair<int,int>>> wordCoords(vector<vector<char>> &grid , vector<string> & words ){
-        vector<vector<pair<int,int>>> ans ; 
-        vector<pair<int,int>> dir ={{-1,0}, {1,0}, {0,1}, {0,-1}}; 
-        int m = grid.size() ; 
-        int n = grid[0].size() ; 
-        for(string str : words ){
-            vector<pair<int,int>> coord ; 
-            vector<vector<int>> vis (m , vector<int>(n,0));
-            bool wordPresent = false ; 
-            for(int i = 0 ; i<m && !wordPresent; i++){
-                for(int j = 0 ; j<n &&!wordPresent ; j++){
-                    int ind = 0 ; 
-                    if(grid[i][j] == str[ind] ){
-                        queue<pair<int,int>> q ; 
-                        vector<pair<int, int>> tempCoord;
-                        tempCoord.push_back({i,j});
-                        q.push({i,j}) ;
-                        vis[i][j] = 1 ;  
-                        ind ++ ; 
-                        while(!q.empty()){
-                            pair<int,int> frCrd = q.front() ;
-                            int x = frCrd.first ; 
-                            int y = frCrd.second ;  
-                            q.pop() ; 
-                            for(pair<int,int> &d  : dir){
-                                int dx = d.first ; 
-                                int dy = d.second ; 
-                                int nx = x +dx ; 
-                                int ny = y+ dy ; 
-                                if(nx>=0 && ny>=0 && nx<m && ny<n && !vis[nx][ny] && grid[nx][ny] == str[ind]){
-                                    q.push({nx,ny});
-                                    tempCoord.push_back({nx, ny});
-                                    vis[nx][ny] = 1 ;
-                                    ind++ ; 
-                                    break ; 
-                                }
+vector<vector<pair<int,int>>> wordCoords(vector<vector<char>> &grid, vector<string> &words) {
+    vector<vector<pair<int,int>>> ans; 
+    vector<pair<int,int>> dir = {{-1,0}, {1,0}, {0,1}, {0,-1}}; 
+    vector<pair<int,int>> diagDir = {{-1,-1}, {-1,1}, {1,-1}, {1,1}};
+    int m = grid.size(); 
+    int n = grid[0].size(); 
+    
+    for(string str : words) {
+        vector<pair<int,int>> coord; 
+        bool wordPresent = false; 
+        
+        for(int i = 0; i < m && !wordPresent; i++) {
+            for(int j = 0; j < n && !wordPresent; j++) {
+                if(grid[i][j] == str[0]) {
+                    // BFS for straight directions
+                    queue<pair<int,int>> q; 
+                    vector<pair<int,int>> tempCoord;
+                    vector<vector<int>> vis(m, vector<int>(n, 0));
+                    q.push({i, j});
+                    vis[i][j] = 1;  
+                    tempCoord.push_back({i, j});
+                    int ind = 1;
+
+                    while(!q.empty() && ind < str.size()) {
+                        pair<int,int> frCrd = q.front();
+                        int x = frCrd.first; 
+                        int y = frCrd.second;  
+                        q.pop(); 
+                        for(pair<int,int> &d : dir) {
+                            int nx = x + d.first; 
+                            int ny = y + d.second; 
+                            if(nx >= 0 && ny >= 0 && nx < m && ny < n && !vis[nx][ny] && grid[nx][ny] == str[ind]) {
+                                q.push({nx, ny});
+                                tempCoord.push_back({nx, ny});
+                                vis[nx][ny] = 1;
+                                ind++; 
+                                break; 
                             }
                         }
-                        if (ind == str.size()) {
-                            coord = tempCoord;
-                            wordPresent = true;
+                    }
+                    if (ind == str.size()) {
+                        coord = tempCoord;
+                        wordPresent = true;
                     } else {
+                        // Check diagonals
+                        for(auto &d : diagDir) {
+                            tempCoord.clear();
+                            tempCoord.push_back({i, j});
+                            int x = i, y = j;
+                            ind = 1;
+                            while(x + d.first >= 0 && x + d.first < m && y + d.second >= 0 && y + d.second < n && grid[x + d.first][y + d.second] == str[ind]) {
+                                x += d.first;
+                                y += d.second;
+                                tempCoord.push_back({x, y});
+                                ind++;
+                                if(ind == str.size()) {
+                                    coord = tempCoord;
+                                    wordPresent = true;
+                                    break;
+                                }
+                            }
+                            if(wordPresent) break;
+                        }
+                    }
+
+                    if(!wordPresent) {
                         coord.clear();
                         coord.push_back({-1, -1});
                     }
-                    }
                 }
-            }ans.push_back(coord) ; 
+            }
         }
-        return ans ;
-    };
-int main(){
-    vector<string> words = {"RED" , 
-        "GARLIC", "SAUSAGE", "CHICKEN","CHEESE",
+        ans.push_back(coord); 
+    }
+    return ans;
+}
+
+int main() {
+    vector<string> words = {
+        "PEPPERONI", "HAM", "PINEAPPLE", "REDSAUCE","RED",
+        "GARLIC", "SAUSAGE", "CHEESE", "CHICKEN" ,"PASTA" // pasta is not present so O/P will be -1
     };
     vector<vector<char>> grid = {
-        {'H', 'C', 'R', 'H', 'A', 'G', 'P', 'A', 'U', 'P'},
-        {'D', 'E', 'E', 'R', 'I', 'S', 'C', 'N', 'L', 'P'},
-        {'S', 'A', 'D', 'P', 'N', 'A', 'H', 'N', 'I', 'A'},
-        {'C', 'L', 'S', 'M', 'O', 'U', 'R', 'N', 'G', 'C'},
-        {'H', 'D', 'A', 'E', 'R', 'S', 'E', 'I', 'A', 'I'},
-        {'I', 'H', 'U', 'H', 'E', 'A', 'H', 'C', 'R', 'E'},
-        {'C', 'E', 'C', 'P', 'P', 'G', 'H', 'O', 'L', 'L'},
-        {'K', 'P', 'E', 'P', 'P', 'E', 'G', 'I', 'I', 'S'},
-        {'E', 'N', 'L', 'N', 'E', 'I', 'S', 'D', 'C', 'C'},
-        {'N', 'E', 'S', 'S', 'P', 'I', 'R', 'I', 'I', 'E'},
-        {'N', 'R', 'E', 'M', 'H', 'S', 'C', 'S', 'A', 'I'}};
-     vector<vector<pair<int,int>>> solution = wordCoords(grid,words) ; 
-    for(auto &vec1 : solution){
-            for(pair<int,int> & crd: vec1){
-                cout<<crd.first;
-                cout<<crd.second;
-                cout<<" ";
+      // 0    1    2    3     4    5   6     7    8   9 
+        {'H', 'C', 'R', 'H', 'A', 'G', 'P', 'A', 'U', 'P'}, //0
+        {'D', 'E', 'E', 'R', 'I', 'S', 'C', 'N', 'L', 'P'}, //1
+        {'S', 'A', 'D', 'P', 'N', 'A', 'H', 'N', 'I', 'A'}, //2
+        {'C', 'L', 'S', 'M', 'O', 'U', 'R', 'N', 'G', 'C'}, //3
+        {'H', 'D', 'A', 'E', 'R', 'S', 'E', 'I', 'A', 'I'}, //4
+        {'I', 'H', 'U', 'H', 'E', 'A', 'H', 'C', 'R', 'E'}, //5
+        {'C', 'E', 'C', 'P', 'P', 'G', 'H', 'O', 'L', 'L'}, //6
+        {'K', 'P', 'E', 'P', 'P', 'E', 'G', 'I', 'I', 'S'}, //7
+        {'E', 'N', 'L', 'N', 'E', 'I', 'S', 'D', 'C', 'C'}, //8
+        {'N', 'E', 'S', 'S', 'P', 'I', 'R', 'I', 'I', 'E'}, //9
+        {'N', 'R', 'E', 'M', 'H', 'S', 'C', 'S', 'A', 'I'}  // 10
+    };
+    
+    vector<vector<pair<int,int>>> solution = wordCoords(grid, words); 
+    for(int i = 0 ; i<solution.size() ; i++) {
+        cout<<words[i];
+        cout<<" ";
+        for(pair<int,int> &crd: solution[i]) {
+            if(crd.first == -1 && crd.second == -1)cout<<" is Not Found " ; 
+            else{
+            cout<<"->";
+            cout<<" ";
+            cout << "(" << crd.first << "," << crd.second << ") ";}
+        }
+        cout << endl;
     }
-    cout<<endl ;
-    }
-        return 0 ; }
+    
+    return 0; 
+}
